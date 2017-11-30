e409b8b3deef49ed33738993ff74a999af52ef08
Reena Patil <reena.patil@outlook.com>
2017-11-30 21:39:58 +1100

    updated todoitem.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
 import React from 'react';
-import TodoItem from './TodoItem'
+import TodoItem from './TodoItem';
 
-const toggleItemAtIndex = (items,indexToToggle)=>{
-  const item = items [indexToToggle];
+const toggleItemAtIndex = (items, indexToToggle) => {
+  const item = items[indexToToggle];
   item.completed = !item.completed;
   return items;
-}
+};
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {items: [
-      {description: 'Go to Gym',completed: true},
-      {description: 'Call to Sara',completed: false},
-      {description: 'Email Kevin',completed: true},
-      {description: 'Complete linkedin profile',completed: false},
-      {description: 'Pay electricity bill',completed: false}
-    ]}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { description: 'Go to Gym', completed: true },
+        { description: 'Call to Sara', completed: false },
+        { description: 'Email Kevin', completed: true },
+        { description: 'Complete linkedin profile', completed: false },
+        { description: 'Pay electricity bill', completed: false }
+      ]
+    };
   }
 
-  onToggleItemAtIndex = (index) => {
-    this.setState((prevState)=>{
+  onToggleItemAtIndex = index => {
+    this.setState(prevState => {
       let items = prevState.items;
-      items = toggleItemAtIndex(items,index);
-      return { items }
-    })
-  }
+      items = toggleItemAtIndex(items, index);
+      return { items };
+    });
+  };
 
-  render(){
+  render() {
     const items = this.state.items;
 
-    return(
+    return (
       <div className="App">
-      {
-        items.map((item,index)=>(
-          <TodoItem key={index}
-          description={item.description}
-          completed={item.completed}
-          onToggle={()=>this.onToggleItemAtIndex(index)}
+        {items.map((item, index) => (
+          <TodoItem
+            key={index}
+            description={item.description}
+            completed={item.completed}
+            onToggle={() => this.onToggleItemAtIndex(index)}
           />
-        ))
-      }
+        ))}
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
-
-
+export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const TodoItem = ({ description, completed, onToggle }) => (
-	<label>
-		<input type ="checkbox" checked={completed} onChange={onToggle}/>
-		{description}
-	</label>
-)
+  <label>
+    <input type="checkbox" checked={completed} onChange={onToggle} />
+    {description}
+  </label>
+);
 
-export default TodoItem
+export default TodoItem;
